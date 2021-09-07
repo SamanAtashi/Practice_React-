045234ee7297999730a498d01b70ebe435837f01
SamanAtashi <milady00126@gmail.com>
2021-09-07 14:09:55 +0300

    Change class components to functional component

--- trial-2-person/src/Components/Cockpit/Cockpit.js
+++ trial-2-person/src/Components/Cockpit/Cockpit.js
@@ -2,32 +2,36 @@ import React from 'react';
 
 import classes from './Cockpit.css';
 
-const Cockpit = (props) => {
-	const assignedClasses = [];
+class Cockpit extends React.Component {
+	render() {
+		const { props } = this;
 
-	let btnClass = '';
-	if (props.showPersons) {
-		btnClass = classes.Red;
-	}
+		const assignedClasses = [];
 
-	if (props.persons.length <= 2) {
-		assignedClasses.push(classes.red); // classes = ['red']
-	}
-	if (props.persons.length <= 1) {
-		assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-	}
+		let btnClass = '';
+		if (props.showPersons) {
+			btnClass = classes.Red;
+		}
 
-	return (
-		<div className={classes.Cockpit}>
-			<h1>Hi, I'm a React App</h1>
-			<p className={assignedClasses.join(' ')}>
-				This is really working!
-			</p>
-			<button className={btnClass} onClick={props.clicked}>
-				Toggle Persons
-			</button>
-		</div>
-	);
-};
+		if (props.persons.length <= 2) {
+			assignedClasses.push(classes.red); // classes = ['red']
+		}
+		if (props.persons.length <= 1) {
+			assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+		}
+
+		return (
+			<div className={classes.Cockpit}>
+				<h1>Hi, I'm a React App</h1>
+				<p className={assignedClasses.join(' ')}>
+					This is really working!
+				</p>
+				<button className={btnClass} onClick={props.clicked}>
+					Toggle Persons
+				</button>
+			</div>
+		);
+	}
+}
 
 export default Cockpit;
--- trial-2-person/src/Components/Persons.js
+++ trial-2-person/src/Components/Persons.js
@@ -2,19 +2,26 @@ import React from 'react';
 
 import Person from './Person/Person';
 
-const Persons = (props) =>
-	props.persons.map((person, index) => {
-		return (
-			<Person
-				key={person.id}
-				click={() => props.clicked(index)}
-				name={person.name}
-				age={person.age}
-				changed={(event) =>
-					props.named(event, person.id)
-				}
-			/>
-		);
-	});
+class Persons extends React.Component {
+    render() {
+        const {
+            props,
+        } = this;
+
+        return props.persons.map((person, index) => {
+            return (
+                <Person
+                    key={person.id}
+                    click={() => props.clicked(index)}
+                    name={person.name}
+                    age={person.age}
+                    changed={(event) =>
+                        props.named(event, person.id)
+                    }
+                />
+            );
+        });
+    }
+}
 
 export default Persons;
