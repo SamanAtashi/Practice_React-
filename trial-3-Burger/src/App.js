81d886bc2c155372899a40c9898dea27e74d593f
SamanAtashi <milady00126@gmail.com>
2021-09-07 18:45:46 +0300

    Add Layout + basic folders

--- trial-3-Burger/src/App.js
+++ trial-3-Burger/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
+import Layout from './Components/Layout/Layout';
+
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+	return (
+		<div>
+			<Layout>
+				<p>test</p>
+			</Layout>
+		</div>
+	);
 }
 
 export default App;
