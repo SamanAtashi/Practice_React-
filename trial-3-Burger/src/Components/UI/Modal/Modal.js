508bc48ac122ef2e189381f7ac1f810e93647a62
SamanAtashi <milady00126@gmail.com>
2021-09-14 18:01:38 +0300

    Add performance improve with shouldComponentUpdate

--- trial-3-Burger/src/Components/UI/Modal/Modal.js
+++ trial-3-Burger/src/Components/UI/Modal/Modal.js
@@ -3,23 +3,34 @@ import BackDrop from '../BackDrop/BackDrop';
 import Aux from '../../../hoc/aux';
 import classes from './Modal.module.css';
 
-const Modal = (props) => {
-	return (
-		<Aux>
-			<BackDrop show={props.show} cancelClick={props.cancelClick} />
-			<div
-				style={{
-					transform: props.show
-						? 'translateY(0)'
-						: 'translateY(-100vh)',
-					opacity: props.show ? '1' : '0',
-				}}
-				className={classes.Modal}
-			>
-				{props.children}
-			</div>
-		</Aux>
-	);
-};
+class Modal extends React.Component {
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.show !== this.props.show;
+	}
+
+	render() {
+		const { props } = this;
+
+		return (
+			<Aux>
+				<BackDrop
+					show={props.show}
+					cancelClick={props.cancelClick}
+				/>
+				<div
+					style={{
+						transform: props.show
+							? 'translateY(0)'
+							: 'translateY(-100vh)',
+						opacity: props.show ? '1' : '0',
+					}}
+					className={classes.Modal}
+				>
+					{props.children}
+				</div>
+			</Aux>
+		);
+	}
+}
 
 export default Modal;
