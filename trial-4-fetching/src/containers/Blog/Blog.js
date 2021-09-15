ffee3d5b325f96dc2a439b9d9db2a527e8aa90b4
SamanAtashi <milady00126@gmail.com>
2021-09-15 08:51:14 +0300

    Add posts dynamically

--- trial-4-fetching/src/containers/Blog/Blog.js
+++ trial-4-fetching/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Children, Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,36 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+	state = {
+		posts: [],
+	};
+
+	componentDidMount() {
+		axios.get('https://jsonplaceholder.typicode.com/posts').then(
+			(res) => {
+				this.setState({ posts: res.data });
+			}
+		);
+	}
+
+	render() {
+		const posts = this.state.posts.map((post) => {
+            console.log(post)
+			return <Post key={post.id} title={post.title} />;
+		});
+
+		return (
+			<div>
+				<section className="Posts">{posts}</section>
+				<section>
+					<FullPost />
+				</section>
+				<section>
+					<NewPost />
+				</section>
+			</div>
+		);
+	}
 }
 
 export default Blog;
