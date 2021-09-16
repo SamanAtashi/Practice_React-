7aae816dde7a2217dd7e4fc0b3e6e47b998215ba
SamanAtashi <milady00126@gmail.com>
2021-09-16 09:08:03 +0300

    Add error catcher after .then method

--- trial-4-fetching/src/containers/Blog/Blog.js
+++ trial-4-fetching/src/containers/Blog/Blog.js
@@ -10,11 +10,12 @@ class Blog extends Component {
 	state = {
 		posts: [],
 		selectedId: null,
+		error: false,
 	};
 
 	componentDidMount() {
-		axios.get('https://jsonplaceholder.typicode.com/posts').then(
-			(res) => {
+		axios.get('https://jsonplaceholder.typicode.com/posts')
+			.then((res) => {
 				const posts = res.data.slice(0, 4);
 				const updatedPosts = posts.map((post) => {
 					return {
@@ -23,8 +24,10 @@ class Blog extends Component {
 					};
 				});
 				this.setState({ posts: updatedPosts });
-			}
-		);
+			})
+			.catch((err) => {
+				this.setState({ error: true });
+			});
 	}
 
 	postSelectedHandler = (id) => {
@@ -32,18 +35,24 @@ class Blog extends Component {
 	};
 
 	render() {
-		const posts = this.state.posts.map((post) => {
-			return (
-				<Post
-					key={post.id}
-					title={post.title}
-					author={post.author}
-					clicked={() => {
-						this.postSelectedHandler(post.id);
-					}}
-				/>
-			);
-		});
+		let posts = (
+			<p style={{ textAlign: 'center' }}>Something went Wrong</p>
+		);
+
+		if (!this.state.error) {
+			posts = this.state.posts.map((post) => {
+				return (
+					<Post
+						key={post.id}
+						title={post.title}
+						author={post.author}
+						clicked={() => {
+							this.postSelectedHandler(post.id);
+						}}
+					/>
+				);
+			});
+		}
 
 		return (
 			<div>
